8d3d5f690ec8e53332ad790ace015abfae433c8b
Jiahua Zhang <jzhang016@cable.comcast.com>
2021-11-30 20:41:20 -0500

    fix download filename issue

--- components/common/DownloadAsJson.tsx
+++ components/common/DownloadAsJson.tsx
@@ -1,7 +1,7 @@
 import { DownloadOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 import moment from 'moment';
-import React from 'react';
+import React, { useRef } from 'react';
 
 interface Props {
   filename: string;
@@ -10,12 +10,18 @@ interface Props {
 
 export const DownloadAsJson = (props: Props) => {
   const { filename, data } = props;
+  const ref = useRef<HTMLLinkElement>(null);
 
   return (
     <Button
+      ref={ref}
       type='primary'
       href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(data))}`}
-      download={`${moment().format('yyyy-MM-DD ddd')} ${filename}.json`}>
+      onClick={() => {
+        console.log(
+          ((ref.current as any).download = `${moment().format('yyyy-MM-DD ddd')} ${filename}.json`)
+        );
+      }}>
       <DownloadOutlined />
     </Button>
   );
